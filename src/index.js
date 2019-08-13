de837679a9b09dd6c8a98a63b0cf72deca226495
Diego Marcillo <da>
2019-08-13 15:03:20 -0500

    segundo commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('Server sdfd!');
+const express = require('express');
+const morgan = require('morgan');
+const path =  require('path');
+
+const app = express();
+
+// settings
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
+
+// middlewares 
+
+// routes
+app.get('/',(req,res)=>{
+     res.render('index',{title: 'Rutas Andaki'});
+  });
+
+
+// static files
+
+// listener in server
+app.listen(app.get('port'), ()=>{
+     console.log('Server on ports '+app.get('port'));
+})
