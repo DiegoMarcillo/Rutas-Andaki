46f4f2d08e08d93ea82014bd98ac194aa693f274
Diego Marcillo <da>
2019-08-14 13:31:18 -0500

    Mas cambios

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res)=>{
-     res.render('index.html',{title: 'Rutas Andaki'});
+     res.render('index.html',{title: 'Rutas Andaki', slogan: 'BIENVENIDOS'});
   });
 
 router.get('/about',(req,res)=>{
-   res.render('about.html',{title: 'Quienes Somos'});
+   res.render('about.html',{title: 'Quienes Somos', mision: 'Una agencia operadora de turismo, que ofrece planes de turismo de naturaleza y aventura en el territorio Andaki, garantizando la satisfacción de los clientes gracias a la vivencia, goce y disfrute responsable de la biodiversidad del Piedemonte andino Amazónico y al profesionalismo del personal altamente calificado.'});
 });
 
 router.get('/contact', (req, res)=>{
